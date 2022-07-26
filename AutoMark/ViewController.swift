2973c1af010bdec8c19f7a44bdee723f12d883d3
skyman <sokdep@yandex.ru>
2022-07-26 23:03:51 +0300

    Initial Commit

---
+++ AutoMark/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AutoMark
+//
+//  Created by Дмитрий Соколов on 26.07.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
