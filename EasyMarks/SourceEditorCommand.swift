b5f22f4cdc365787c744008968d8f89489f2a392
skyman <sokdep@yandex.ru>
2022-07-28 01:51:52 +0300

    fixes

--- EasyMarks/SourceEditorCommand.swift
+++ EasyMarks/SourceEditorCommand.swift
@@ -9,9 +9,11 @@ import Foundation
 import XcodeKit
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
-    
+    private var cachedLines: NSMutableArray = []
+
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         defer { completionHandler(nil) }
+
         var regex: NSRegularExpression?
         do {
              regex = try NSRegularExpression(
@@ -22,29 +24,51 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         catch {
             fatalError("Can't create regular expression")
         }
+
         guard let regex = regex else {
             completionHandler(nil)
             return
         }
 
         let lines = invocation.buffer.lines
+
         for line in lines {
+            // since we insert new lines with Marks above reference line we need to check
+            //  in order not to write new mark comment (fix later with jumping to next line)
+            guard !cachedLines.contains(line) else {
+                continue
+            }
+//            let index = lines.index(of: line)
+//            let prevLine = lines[index - 1]
+
             if let l = line as? String {
-                let matches = regex.matches(
+                let matches = regex.firstMatch(
                     in: l,
-                    options: [],
-                    range: NSRange(location: 0, length: l.utf8.count)
+                    range: NSRange(location: 0, length: l.utf16.count)
                 )
-                if let match = matches.first {
+
+                if let match = matches {
                     let range = match.range(at: 1)
                     if let swiftRange = Range(range, in: l) {
-                        let name = l[swiftRange]
+                        var name = String(l[swiftRange])
+                        clearNameIfNeeded(str: &name)
+
                         let index = lines.index(of: line)
                         lines.insert("// MARK: - \(name)", at: index)
+                        cachedLines.add(line)
                     }
                 }
             }
         }
         completionHandler(nil)
+    }
+
+    private func clearNameIfNeeded(str: inout String) {
+        if let index = str.firstIndex(of: "{") {
+            str.remove(at: index)
+        }
+        if let index = str.firstIndex(of: "}") {
+            str.remove(at: index)
+        }
     }
 }
